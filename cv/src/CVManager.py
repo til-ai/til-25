18d882e3095b009aebf0d6dd69d763c65cb4482a
Ada Ho <25481871+HoWingYip@users.noreply.github.com>
2025-05-01 17:27:56 +0800

    Include category_id in CV prediction

--- cv/src/CVManager.py
+++ cv/src/CVManager.py
@@ -1,3 +1,6 @@
+from typing import Dict, List
+
+
 class CVManager:
     def __init__(self):
         """
@@ -5,8 +8,22 @@ class CVManager:
         initialize your model and any static configurations.
         """
 
-    def identify(self, image: bytes) -> list[list[int]]:
+    def identify(self, image: bytes) -> List[Dict[str, int | List[int]]]:
         """
-        Performs object detection, returning a list of bounding boxes.
+        Performs object detection, returning a list of predictions in the format
+        [
+            {
+                "bbox": [x, y, w, h],
+                "category_id": category_id
+            },
+            ...
+        ]
+        where:
+        - (x, y) are the coordinates of the top left corner of the predicted bounding box,
+        - (w, h) are its width and height in pixels,
+        - category_id is the index of the predicted category of the enclosed object.
+
+        Note that all bounding box coordinates are 0-indexed. That is, if (x, y) = (0, 0),
+        then the top left corner of your bounding box is the top left corner of the image.
         """
         return []
